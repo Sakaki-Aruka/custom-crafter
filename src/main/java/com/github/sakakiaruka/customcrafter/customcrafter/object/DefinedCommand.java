8c5177b4127fcb1976745478b7fa8fde5449e339
SakakiAruka <yshida1pc7@gmail.com>
2023-06-26 19:31:55 +0900

    update: command checker update

--- src/main/java/com/github/sakakiaruka/customcrafter/customcrafter/object/DefinedCommand.java
+++ src/main/java/com/github/sakakiaruka/customcrafter/customcrafter/object/DefinedCommand.java
@@ -14,4 +14,36 @@ public class DefinedCommand {
         this.argsLen = argsLen;
         this.args = args;
     }
+
+    public String getCommandName() {
+        return commandName;
+    }
+
+    public void setCommandName(String commandName) {
+        this.commandName = commandName;
+    }
+
+    public boolean isHasArgs() {
+        return hasArgs;
+    }
+
+    public void setHasArgs(boolean hasArgs) {
+        this.hasArgs = hasArgs;
+    }
+
+    public int getArgsLen() {
+        return argsLen;
+    }
+
+    public void setArgsLen(int argsLen) {
+        this.argsLen = argsLen;
+    }
+
+    public List<String> getArgs() {
+        return args;
+    }
+
+    public void setArgs(List<String> args) {
+        this.args = args;
+    }
 }
--- src/main/java/com/github/sakakiaruka/customcrafter/customcrafter/util/DefinedCommandUtil.java
+++ src/main/java/com/github/sakakiaruka/customcrafter/customcrafter/util/DefinedCommandUtil.java
@@ -2,9 +2,14 @@ package com.github.sakakiaruka.customcrafter.customcrafter.util;
 
 import com.github.sakakiaruka.customcrafter.customcrafter.CustomCrafter;
 import com.github.sakakiaruka.customcrafter.customcrafter.object.DefinedCommand;
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 
 import java.util.*;
+
+import static com.github.sakakiaruka.customcrafter.customcrafter.SettingsLoad.*;
+import static com.github.sakakiaruka.customcrafter.customcrafter.util.RecipePermissionUtil.recipePermissionMap;
 
 public class DefinedCommandUtil {
     public static Map<Integer,List<DefinedCommand>> definedCommands = new HashMap<>();
@@ -35,7 +40,40 @@ public class DefinedCommandUtil {
         if (!definedCommands.containsKey(input.size())) return false; //invalid args
 
         for(DefinedCommand command : definedCommands.get(input.size())) {
-            //
+            for (int i=0;i<command.getArgsLen();i++) {
+                if (command.getArgs().get(i).equals("{Player}")) {
+                    Player dummy;
+                    try{
+                        dummy = Bukkit.getPlayer(input.get(i));
+                    }catch (Exception e) {
+                        return false;
+                    }
+                    if (!Bukkit.getOnlinePlayers().contains(dummy)) return false;
+                }
+
+                if (command.getArgs().get(i).equals("{Matter}")) {
+                    if (!matters.keySet().contains(input.get(i))) return false;
+                }
+
+                if (command.getArgs().get(i).equals("{Result}")) {
+                    if (!results.keySet().contains(input.get(i))) return false;
+                }
+
+                if (command.getArgs().get(i).equals("{Recipe}")) {
+                    if (!namedRecipes.containsKey(input.get(i))) return false;
+                }
+
+                if (command.getArgs().get(i).equals("{RecipePermission}")) {
+                    if (!recipePermissionMap.keySet().contains(input.get(i))) return false;
+                }
+
+                if (command.getArgs().get(i).startsWith("-")) {
+                    if (!command.getArgs().get(i).equalsIgnoreCase(input.get(i))) return false;
+                }
+
+                if (!command.getArgs().get(i).equals(input.get(i))) return false;
+            }
         }
+        return true;
     }
 }
