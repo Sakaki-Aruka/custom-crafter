107cb08823295c9ec170e3410d8bb270a3cb8f9a
Aruka <99568054+Sakaki-Aruka@users.noreply.github.com>
2024-03-04 13:45:57 +0900

    fix : code format, some logic

--- src/main/java/com/github/sakakiaruka/customcrafter/customcrafter/listener/ModifyCraftingInventory.java
+++ src/main/java/com/github/sakakiaruka/customcrafter/customcrafter/listener/ModifyCraftingInventory.java
@@ -27,86 +27,64 @@ public class ModifyCraftingInventory implements Listener {
     public void onInventoryClick(InventoryClickEvent event){
         Player player = Bukkit.getPlayer(event.getWhoClicked().getUniqueId());
 
-        if(!opening.contains(player))return;
+        if (player == null) return;
+        if (!opening.contains(player)) return;
 
         int slot = event.getRawSlot();
-        if(slot < 0){
+        if (slot < 0){
             event.setCancelled(true);
             return;
         }
-
 
         ClickType clickType = event.getClick();
-        if(slot >= CRAFTING_TABLE_TOTAL_SIZE){
+        if (slot >= CRAFTING_TABLE_TOTAL_SIZE) {
             // click players inventory
-            if(clickType.equals(ClickType.LEFT))return;
-            if(clickType.equals(ClickType.RIGHT))return;
-            if(clickType.equals(ClickType.SHIFT_LEFT))return;
+            if (clickType.equals(ClickType.LEFT) || clickType.equals(ClickType.RIGHT) || clickType.equals(ClickType.SHIFT_LEFT)) return;
             event.setCancelled(true);
             return;
         }
 
-        Inventory inventory = event.getClickedInventory();
-        if (slot == CRAFTING_TABLE_MAKE_BUTTON){
+        Inventory inventory;
+        if ((inventory  = event.getClickedInventory()) == null) return;
+        if (slot == CRAFTING_TABLE_MAKE_BUTTON) {
             // click make button
             event.setCancelled(true);
             // <=== empty checker
             List<ItemStack> forCheck = new ArrayList<>();
-            for(int i: InventoryUtil.getTableSlots(CRAFTING_TABLE_SIZE)){
-                if(inventory.getItem(i) == null)continue;
+            for(int i : InventoryUtil.getTableSlots(CRAFTING_TABLE_SIZE)) {
+                if (inventory.getItem(i) == null) continue;
                 forCheck.add(inventory.getItem(i));
             }
-            if(forCheck.isEmpty())return;
+            if (forCheck.isEmpty()) return;
             // empty checker ===>
-            // replace old result items
-            if(inventory.getItem(CRAFTING_TABLE_RESULT_SLOT) != null){
-
+            if (inventory.getItem(CRAFTING_TABLE_RESULT_SLOT) != null) {
+                // replace old result items
                 InventoryUtil.safetyItemDrop(player, Collections.singletonList(inventory.getItem(CRAFTING_TABLE_RESULT_SLOT)));
-                inventory.setItem(CRAFTING_TABLE_RESULT_SLOT,new ItemStack(Material.AIR));
+                inventory.setItem(CRAFTING_TABLE_RESULT_SLOT, new ItemStack(Material.AIR));
             }
-            if(clickType.equals(ClickType.RIGHT)){
+
+            if (clickType.equals(ClickType.SHIFT_LEFT)) {
                 // mass
                 new Search().massSearch(player,inventory,false);
                 // result item is null
-                if(WHAT_MAKING.get(player.getUniqueId()) == null)return;
-                int minimal = getMinimalAmount(inventory);
-
-            }
-            else if(clickType.equals(ClickType.LEFT)){
+                if (WHAT_MAKING.get(player.getUniqueId()) == null) return;
+            } else if(clickType.equals(ClickType.LEFT)) {
                 // normal
                 new Search().massSearch(player,inventory,true);
                 // result item is null
-                if(WHAT_MAKING.get(player.getUniqueId()) == null)return;
+                if (WHAT_MAKING.get(player.getUniqueId()) == null) return;
             }
-
-
-        }else if (slot == CRAFTING_TABLE_RESULT_SLOT){
+        } else if (slot == CRAFTING_TABLE_RESULT_SLOT) {
             // click result slot
             event.setCancelled(true);
-            if(inventory.getItem(CRAFTING_TABLE_RESULT_SLOT) == null)return;
+            if (inventory.getItem(CRAFTING_TABLE_RESULT_SLOT) == null) return;
             InventoryUtil.decrementResult(inventory,player); // remove result that clicked.
-
-        }else if(InventoryUtil.getBlankCoordinates(CRAFTING_TABLE_SIZE).contains(slot)){
+        } else if (InventoryUtil.getBlankCoordinates(CRAFTING_TABLE_SIZE).contains(slot)) {
             // click a blank slot
             event.setCancelled(true);
             return;
         }
-
         // init making data.
-        WHAT_MAKING.put(player.getUniqueId(),null);
+        WHAT_MAKING.put(player.getUniqueId(), null);
     }
-
-    private int getMinimalAmount(Inventory inventory){
-        List<Integer> list = new ArrayList<>();
-        for(int i: InventoryUtil.getTableSlots(CRAFTING_TABLE_SIZE)){
-            if(inventory.getItem(i) == null)continue;
-            if(inventory.getItem(i).getType().equals(Material.AIR))continue;
-            list.add(inventory.getItem(i).getAmount());
-
-        }
-        if(list.isEmpty())return -1;
-        Collections.sort(list);
-        return list.get(0);
-    }
-
 }
