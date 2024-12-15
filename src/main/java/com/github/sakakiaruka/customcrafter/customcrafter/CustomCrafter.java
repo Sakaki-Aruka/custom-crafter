f6ba735e041325cae16a3244eae0ba6dc6367d9b
Sakaki Aruka <99568054+Sakaki-Aruka@users.noreply.github.com>
2024-12-16 03:57:24 +0900

    delete: needless files

--- src/main/java/com/github/sakakiaruka/customcrafter/customcrafter/CustomCrafter.java
+++ src/main/java/com/github/sakakiaruka/customcrafter/customcrafter/CustomCrafter.java
@@ -1,16 +1,7 @@
 package com.github.sakakiaruka.customcrafter.customcrafter;
 
 import com.github.sakakiaruka.customcrafter.customcrafter.api.CustomCrafterAPI;
-import com.github.sakakiaruka.customcrafter.customcrafter.command.HistoryDatabase;
-import com.github.sakakiaruka.customcrafter.customcrafter.command.Processor;
-import com.github.sakakiaruka.customcrafter.customcrafter.listener.Listener;
-import com.github.sakakiaruka.customcrafter.customcrafter.util.PlaceholderUtil;
-import com.github.sakakiaruka.customcrafter.customcrafter.util.RecipePermissionUtil;
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public final class CustomCrafter extends JavaPlugin {
 
@@ -26,25 +17,10 @@ public final class CustomCrafter extends JavaPlugin {
 
         INITIALIZED = System.currentTimeMillis();
         CustomCrafterAPI.INSTANCE.setup$custom_crafter();
-
-//        new SettingsLoad().load();
-//        getCommand("cc").setExecutor(new Processor());
-//        getCommand("history_database").setExecutor(HistoryDatabase.INSTANCE);
-//        getServer().getPluginManager().registerEvents(new Listener(),this);
-//        ENABLED_PLACEHOLDER_API = Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null;
-//        if (ENABLED_PLACEHOLDER_API) {
-//            new PlaceholderUtil().register();
-//        }
     }
 
     @Override
-    public void onDisable() {
-//        // Plugin shutdown logic
-//        if (getConfig().contains("permissions") && getConfig().contains("relate")) {
-//            Path path = Paths.get(getConfig().getString("relate"));
-//            RecipePermissionUtil.playerPermissionWriter(path);
-//        }
-    }
+    public void onDisable() {}
 
     public static CustomCrafter getInstance(){
         return instance;
--- src/main/java/com/github/sakakiaruka/customcrafter/customcrafter/interfaces/TriConsumer.java
+++
@@ -1,6 +0,0 @@
-package com.github.sakakiaruka.customcrafter.customcrafter.interfaces;
-
-@FunctionalInterface
-public interface TriConsumer<T, U, V> {
-    public void accept(T t, U u, V v);
-}
