e17e9e00abd1792022d72c17ee6a5cba67e45169
Aruka <99568054+Sakaki-Aruka@users.noreply.github.com>
2024-12-16 03:59:38 +0900

    delete: needless files (#201)

--- src/main/java/com/github/sakakiaruka/customcrafter/customcrafter/interfaces/PentaConsumer.java
+++
@@ -1,6 +0,0 @@
-package com.github.sakakiaruka.customcrafter.customcrafter.interfaces;
-
-@FunctionalInterface
-public interface PentaConsumer<A, B, C, D, E> {
-    void accept(A a, B b, C c, D d, E e);
-}
