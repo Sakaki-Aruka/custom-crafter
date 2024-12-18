01e2cdfedce43c68a3a0ea3c8a653863b3f2ba3f
SakakiAruka <yshida1pc7@gmail.com>
2024-12-19 01:37:25 +0900

    CMatterPredicate

--- src/main/java/com/github/sakakiaruka/customcrafter/customcrafter/api/object/matter/CMatterPredicate.kt
+++ src/main/java/com/github/sakakiaruka/customcrafter/customcrafter/api/object/matter/CMatterPredicate.kt
@@ -9,24 +9,35 @@ import java.util.UUID
 /**
  * A [CMatter]'s predicate functions.
  */
-data class CMatterPredicate private constructor(
-    val predicate: MatterPredicateFunc
-){
-    fun interface MatterPredicateFunc {
-        fun invoke(
-            self: ItemStack,
-            mapped: Map<CoordinateComponent, ItemStack>,
-            recipe: CRecipe,
-            crafterID: UUID
-        ): Boolean
-    }
+data class CMatterPredicate(
+    val predicate: (ItemStack, Map<CoordinateComponent, ItemStack>, CRecipe, UUID) -> Boolean) {
 
-    companion object {
-        fun of(func: (self: ItemStack,
-                      mapped: Map<CoordinateComponent, ItemStack>,
-                      recipe: CRecipe,
-                      crafterID: UUID) -> Boolean): CMatterPredicate {
-            return CMatterPredicate(MatterPredicateFunc(func))
-        }
-    }
+    operator fun invoke(
+        self: ItemStack,
+        mapped: Map<CoordinateComponent, ItemStack>,
+        recipe: CRecipe,
+        crafterID: UUID
+    ): Boolean = predicate(self, mapped, recipe, crafterID)
 }
+
+//data class CMatterPredicate private constructor(
+//    val predicate: MatterPredicateFunc
+//){
+//    fun interface MatterPredicateFunc {
+//        fun invoke(
+//            self: ItemStack,
+//            mapped: Map<CoordinateComponent, ItemStack>,
+//            recipe: CRecipe,
+//            crafterID: UUID
+//        ): Boolean
+//    }
+//
+//    companion object {
+//        fun of(func: (self: ItemStack,
+//                      mapped: Map<CoordinateComponent, ItemStack>,
+//                      recipe: CRecipe,
+//                      crafterID: UUID) -> Boolean): CMatterPredicate {
+//            return CMatterPredicate(MatterPredicateFunc(func))
+//        }
+//    }
+//}
