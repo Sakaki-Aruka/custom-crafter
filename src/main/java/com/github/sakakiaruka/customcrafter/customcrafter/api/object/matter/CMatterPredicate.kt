45b4e17d5acb7065b8de89e9df59b0c614ecbf1e
SakakiAruka <yshida1pc7@gmail.com>
2024-12-18 21:31:07 +0900

    fix: CMatterPredicate

--- src/main/java/com/github/sakakiaruka/customcrafter/customcrafter/api/object/matter/CMatterPredicate.kt
+++ src/main/java/com/github/sakakiaruka/customcrafter/customcrafter/api/object/matter/CMatterPredicate.kt
@@ -10,11 +10,26 @@ import java.util.UUID
 /**
  * A [CMatter]'s predicate functions.
  */
-data class CMatterPredicate(
-    val predicate: (
-        self: ItemStack,
-        mapped: Map<CoordinateComponent, ItemStack>,
-        container: PersistentDataContainer,
-        recipe: CRecipe,
-        crafterID: UUID) -> Boolean
-)
+data class CMatterPredicate private constructor(
+    val predicate: MatterPredicateFunc
+){
+    fun interface MatterPredicateFunc {
+        fun invoke(
+            self: ItemStack,
+            mapped: Map<CoordinateComponent, ItemStack>,
+            container: PersistentDataContainer,
+            recipe: CRecipe,
+            crafterID: UUID
+        ): Boolean
+    }
+
+    companion object {
+        fun of(func: (self: ItemStack,
+                      mapped: Map<CoordinateComponent, ItemStack>,
+                      container: PersistentDataContainer,
+                      recipe: CRecipe,
+                      crafterID: UUID) -> Boolean): CMatterPredicate {
+            return CMatterPredicate(MatterPredicateFunc(func))
+        }
+    }
+}
