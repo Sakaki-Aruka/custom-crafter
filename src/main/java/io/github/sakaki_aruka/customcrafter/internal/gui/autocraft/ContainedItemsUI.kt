bfe3d3ea34029e0347f1c4ccb44ac8f08dff1196
SakakiAruka <yshida1pc7@gmail.com>
2025-09-01 02:12:58 +0900

    add: ContainedItems UI and test

---
+++ src/main/java/io/github/sakaki_aruka/customcrafter/internal/gui/autocraft/ContainedItemsUI.kt
@@ -0,0 +1,162 @@
+package io.github.sakaki_aruka.customcrafter.internal.gui.autocraft
+
+import io.github.sakaki_aruka.customcrafter.api.interfaces.recipe.AutoCraftRecipe
+import io.github.sakaki_aruka.customcrafter.impl.util.Converter
+import io.github.sakaki_aruka.customcrafter.impl.util.Converter.toComponent
+import io.github.sakaki_aruka.customcrafter.internal.autocrafting.CBlock
+import io.github.sakaki_aruka.customcrafter.internal.autocrafting.CBlockDB
+import io.github.sakaki_aruka.customcrafter.internal.gui.CustomCrafterUI
+import net.kyori.adventure.text.Component
+import org.bukkit.Bukkit
+import org.bukkit.Location
+import org.bukkit.Material
+import org.bukkit.event.inventory.InventoryAction
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.event.inventory.InventoryCloseEvent
+import org.bukkit.inventory.Inventory
+import org.bukkit.inventory.InventoryHolder
+import org.bukkit.inventory.ItemStack
+import java.util.Collections
+import kotlin.math.min
+
+internal class ContainedItemsUI private constructor(
+    val placeableSlots: Set<Int>,
+    private val location: Location
+): CustomCrafterUI, InventoryHolder {
+    private val inventory: Inventory = Bukkit.createInventory(
+        this,
+        54,
+        "<b>Contained Items".toComponent()
+    )
+
+    init {
+        ((0..<54) - placeableSlots).forEach { i ->
+            this.inventory.setItem(i, BLANK)
+        }
+    }
+
+    companion object {
+        val BLANK: ItemStack = ItemStack.of(Material.BLACK_STAINED_GLASS_PANE).apply {
+            itemMeta = itemMeta.apply {
+                displayName(Component.empty())
+            }
+        }
+        private val INV_CACHE: MutableMap<Location, ContainedItemsUI> = Collections.synchronizedMap<Location, ContainedItemsUI>(mutableMapOf())
+
+        fun of(cBlock: CBlock, createNewIfNotExist: Boolean = true): ContainedItemsUI? {
+            return synchronized(INV_CACHE) {
+                INV_CACHE[cBlock.block.location]?.let { ui -> return@synchronized ui }
+
+                if (!createNewIfNotExist) {
+                    return null
+                }
+
+                val recipe: AutoCraftRecipe = cBlock.getRecipe() ?: run {
+                    throw IllegalArgumentException("Could not get a cBlock-contained recipe. 'cBlock#getRecipe' returns null.")
+                }
+
+                val placeableSlots: Set<Int> = recipe.items.keys.map { c -> c.toIndex() }.toSet()
+                val ui = ContainedItemsUI(placeableSlots, cBlock.block.location)
+                INV_CACHE[cBlock.block.location] = ui
+                ui
+            }
+        }
+
+        fun contains(location: Location): Boolean {
+            return synchronized(INV_CACHE) {
+                INV_CACHE.keys.contains(location)
+            }
+        }
+
+        fun clear() {
+            synchronized(INV_CACHE) {
+                INV_CACHE.clear()
+            }
+        }
+    }
+
+    override fun onClose(event: InventoryCloseEvent) {
+        if ((event.inventory.viewers - event.player).isEmpty()) {
+            synchronized(INV_CACHE) {
+                INV_CACHE.remove(this.location)
+            }
+
+            if (CBlockDB.isLinked(this.location.block)) {
+                CBlockDB.clearContainedItems(this.location.block)
+                CBlockDB.addItems(this.location.block, *getNoBlankItems().toTypedArray())
+            }
+        }
+    }
+
+    override fun onClick(
+        clicked: Inventory,
+        event: InventoryClickEvent
+    ) {
+        if (event.isCancelled) {
+            return
+        } else if (event.rawSlot !in Converter.getAvailableCraftingSlotIndices()) {
+            event.isCancelled = true
+            return
+        }
+    }
+
+
+    override fun onPlayerInventoryClick(
+        clicked: Inventory,
+        event: InventoryClickEvent
+    ) {
+        if (event.isCancelled
+            || event.action != InventoryAction.MOVE_TO_OTHER_INVENTORY
+            || event.currentItem == null
+            || event.currentItem!!.isEmpty) {
+            return
+        }
+
+        event.isCancelled = true
+        event.currentItem = merge(event.currentItem!!)
+    }
+
+    /**
+     * @suppress
+     * returns = Remaining Item
+      */
+    fun merge(item: ItemStack): ItemStack {
+        val sourceCloned: ItemStack = item.clone()
+
+        for (slot in Converter.getAvailableCraftingSlotIndices()) {
+            val slotCloned: ItemStack = this.inventory.getItem(slot) ?: run {
+                this.inventory.setItem(slot, sourceCloned)
+                return ItemStack.empty()
+            }
+
+            if (!slotCloned.asOne().isSimilar(sourceCloned)) {
+                continue
+            }
+
+            if (slotCloned.amount >= slotCloned.maxStackSize) {
+                continue
+            }
+
+            val canAddAmount: Int = slotCloned.maxStackSize - slotCloned.amount
+            val addAmount: Int = min(canAddAmount, sourceCloned.amount)
+            if (addAmount <= 0) {
+                continue
+            }
+            this.inventory.setItem(slot, slotCloned.asQuantity(slotCloned.amount + addAmount))
+            sourceCloned.amount -= addAmount
+            if (sourceCloned.amount <= 0) {
+                return ItemStack.empty()
+            }
+        }
+
+        return sourceCloned.takeIf { i -> !i.isEmpty && i.amount > 0 } ?: ItemStack.empty()
+    }
+
+    fun getNoBlankItems(): List<ItemStack> {
+        return this.placeableSlots.sorted().map { i ->
+            this.inventory.getItem(i)?.clone() ?: ItemStack.empty()
+        }
+    }
+
+    override fun getInventory(): Inventory = inventory
+}
---
+++ src/test/kotlin/online/aruka/customcrafter/api/ui/ContainedItemsUITest.kt
@@ -0,0 +1,176 @@
+package online.aruka.customcrafter.api.ui
+
+import io.github.sakaki_aruka.customcrafter.CustomCrafterAPI
+import io.github.sakaki_aruka.customcrafter.api.objects.recipe.CRecipeType
+import io.github.sakaki_aruka.customcrafter.api.objects.recipe.CoordinateComponent
+import io.github.sakaki_aruka.customcrafter.impl.matter.CMatterImpl
+import io.github.sakaki_aruka.customcrafter.impl.recipe.AutoCraftRecipeImpl
+import io.github.sakaki_aruka.customcrafter.internal.autocrafting.CBlock
+import io.github.sakaki_aruka.customcrafter.internal.autocrafting.CBlockDB
+import io.github.sakaki_aruka.customcrafter.internal.gui.autocraft.ContainedItemsUI
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.mockbukkit.mockbukkit.MockBukkit
+import org.mockbukkit.mockbukkit.ServerMock
+import org.mockbukkit.mockbukkit.world.WorldMock
+import kotlin.test.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+object ContainedItemsUITest {
+    private lateinit var server: ServerMock
+
+    @BeforeEach
+    fun setup() {
+        server = MockBukkit.mock()
+        server.addWorld(WorldMock())
+        server.addPlayer()
+
+        CustomCrafterAPI.setAutoCraftingBaseBlock(Material.GOLD_BLOCK)
+
+        server.worlds.first().let { world ->
+            val base = world.getBlockAt(0, 64, 0)
+            base.type = Material.CRAFTER
+
+            for (dx in (-1..1)) {
+                for (dz in (-1..1)) {
+                    if (dx == 0 && dz == 0) {
+                        continue
+                    }
+                    base.getRelative(dx, -1, dz).type = Material.GOLD_BLOCK
+                }
+            }
+        }
+
+        CBlockDB.initTables(useInMemoryDatabase = true)
+        try {
+            CBlockDB.unlink(server.worlds.first().getBlockAt(0, 64, 0))
+        } catch (_: Exception) {
+            //
+        }
+    }
+
+    @AfterEach
+    fun tearDown() {
+        val recipes = CustomCrafterAPI.getRecipes()
+        recipes.forEach { r -> CustomCrafterAPI.unregisterRecipe(r) }
+        try {
+            CBlockDB.unlink(server.worlds.first().getBlockAt(0, 64, 0))
+        } catch (_: Exception) {
+            //
+        }
+
+        MockBukkit.unmock()
+    }
+
+    @Test
+    fun getSameViewTest() {
+        val block = server.worlds.first().getBlockAt(0, 64, 0)
+        val matter = CMatterImpl.single(Material.STONE)
+        val items = CoordinateComponent.square(3).associateWith { c -> matter }
+        val recipe = AutoCraftRecipeImpl(
+            name = "",
+            items = items,
+            type = CRecipeType.NORMAL,
+            publisherPluginName = "Custom_Crafter"
+        )
+        val cBlock = CBlock(
+            version = CustomCrafterAPI.API_VERSION,
+            type = recipe.type,
+            name = recipe.name,
+            publisherName = recipe.publisherPluginName,
+            slots = recipe.items.keys.map { c -> c.toIndex() }.toList(),
+            block = block
+        )
+        CustomCrafterAPI.registerRecipe(recipe)
+
+        val ui1 = ContainedItemsUI.of(cBlock)
+        val ui2 = ContainedItemsUI.of(cBlock)
+        assertTrue(ui1 == ui2)
+    }
+
+    @Test
+    fun itemMergeTest() {
+        val block = server.worlds.first().getBlockAt(0, 64, 0)
+        val matter = CMatterImpl.single(Material.STONE)
+        val items = CoordinateComponent.square(3).associateWith { c -> matter }
+        val recipe = AutoCraftRecipeImpl(
+            name = "",
+            items = items,
+            type = CRecipeType.NORMAL,
+            publisherPluginName = "Custom_Crafter"
+        )
+        val cBlock = CBlock(
+            version = CustomCrafterAPI.API_VERSION,
+            type = recipe.type,
+            name = recipe.name,
+            publisherName = recipe.publisherPluginName,
+            slots = recipe.items.keys.map { c -> c.toIndex() }.toList(),
+            block = block
+        )
+        CustomCrafterAPI.registerRecipe(recipe)
+
+        val ui = ContainedItemsUI.of(cBlock)
+        assertTrue(ui != null)
+        val maxStackStone = ItemStack.of(Material.STONE, 64)
+        // recipe shape
+        // ###
+        // # #
+        // ### -> the limit is 8 slots
+        assertTrue(ui.merge(maxStackStone).isEmpty)
+        assertTrue(ui.merge(maxStackStone).isEmpty)
+        assertTrue(ui.merge(maxStackStone).isEmpty)
+        assertTrue(ui.merge(maxStackStone).isEmpty)
+        assertTrue(ui.merge(maxStackStone).isEmpty)
+        assertTrue(ui.merge(maxStackStone).isEmpty)
+        assertTrue(ui.merge(maxStackStone).isEmpty)
+        assertTrue(ui.merge(maxStackStone).isEmpty)
+
+        // there is no 9th slot. So, 'merge' method returns an item what same with a given.
+        assertTrue(ui.merge(maxStackStone).isSimilar(maxStackStone))
+
+        assertTrue(ui.getNoBlankItems().size == 8)
+        assertTrue(ui.getNoBlankItems().all { item -> item.isSimilar(maxStackStone) })
+    }
+
+    @Test
+    fun itemMergeTest2() {
+        val block = server.worlds.first().getBlockAt(0, 64, 0)
+        val matter = CMatterImpl.single(Material.STONE)
+        val items = CoordinateComponent.square(3).associateWith { c -> matter }
+        val recipe = AutoCraftRecipeImpl(
+            name = "",
+            items = items,
+            type = CRecipeType.NORMAL,
+            publisherPluginName = "Custom_Crafter"
+        )
+        val cBlock = CBlock(
+            version = CustomCrafterAPI.API_VERSION,
+            type = recipe.type,
+            name = recipe.name,
+            publisherName = recipe.publisherPluginName,
+            slots = recipe.items.keys.map { c -> c.toIndex() }.toList(),
+            block = block
+        )
+        CustomCrafterAPI.registerRecipe(recipe)
+
+        val ui = ContainedItemsUI.of(cBlock)
+        assertTrue(ui != null)
+
+        assertTrue(ui.merge(ItemStack.of(Material.STONE, 63)).isEmpty)
+        assertTrue(ui.merge(ItemStack.of(Material.STONE, 63)).isEmpty)
+        assertTrue(ui.merge(ItemStack.of(Material.STONE, 63)).isEmpty)
+        assertTrue(ui.merge(ItemStack.of(Material.STONE, 63)).isEmpty)
+        assertTrue(ui.merge(ItemStack.of(Material.STONE, 63)).isEmpty)
+        assertTrue(ui.merge(ItemStack.of(Material.STONE, 63)).isEmpty)
+        assertTrue(ui.merge(ItemStack.of(Material.STONE, 63)).isEmpty)
+        assertTrue(ui.merge(ItemStack.of(Material.STONE, 63)).isEmpty)
+
+        // over 1 item
+        val over = ui.merge(ItemStack.of(Material.STONE, 9))
+        assertFalse(over.isEmpty)
+        assertTrue(over.amount == 1)
+    }
+}
