2028c58e26d1de7b05dab7f2bfb616d24be0f05a
Sakaki-Aruka <aruka.works@gmail.com>
2025-11-21 01:49:31 +0900

    add: new recipe

--- demo/src/main/kotlin/online/aruka/demo/Demo.kt
+++ demo/src/main/kotlin/online/aruka/demo/Demo.kt
@@ -5,6 +5,7 @@ import online.aruka.demo.listener.CreateCustomItemListener
 import online.aruka.demo.listener.CustomCrafterAPIPropertiesChangeListener
 import online.aruka.demo.listener.CustomItemRegisteredListener
 import online.aruka.demo.recipe.shaped.OverLimitEnchantedBook
+import online.aruka.demo.recipe.shapeless.DyeMixin
 import online.aruka.demo.register.ShapedRecipeProvider
 import online.aruka.demo.register.ShapelessRecipeProvider
 import org.bukkit.Bukkit
@@ -39,6 +40,7 @@ class Demo : JavaPlugin() {
         CustomCrafterAPI.registerRecipe(ShapelessRecipeProvider.glowBerry())
         CustomCrafterAPI.registerRecipe(ShapelessRecipeProvider.infinityIronBlockExtract())
         CustomCrafterAPI.registerRecipe(ShapelessRecipeProvider.extractPotion())
+        CustomCrafterAPI.registerRecipe(DyeMixin.mixWithoutBlack())
     }
 
     override fun onDisable() {
